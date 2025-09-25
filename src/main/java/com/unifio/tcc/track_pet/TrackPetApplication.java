b21e12ab44b61d3a3a17f4dc08890a7fa90e423a
Gabriel Messias <gamessiasjt@gmail.com>
2025-09-24 21:09:42 -0300

    subindo projeto

---
+++ src/main/java/com/unifio/tcc/track_pet/TrackPetApplication.java
@@ -0,0 +1,13 @@
+package com.unifio.tcc.track_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifio/tcc/track_pet/TrackPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifio.tcc.track_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
