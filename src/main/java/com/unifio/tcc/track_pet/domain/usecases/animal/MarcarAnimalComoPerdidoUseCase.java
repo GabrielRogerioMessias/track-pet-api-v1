2903bf54ca72bc09e660051834cc127dba24f687
Gabriel Messias <gamessiasjt@gmail.com>
2025-10-14 19:50:17 -0300

    feat: removendo imports desnecessários

--- src/main/java/com/unifio/tcc/track_pet/domain/usecases/animal/MarcarAnimalComoPerdidoUseCase.java
+++ src/main/java/com/unifio/tcc/track_pet/domain/usecases/animal/MarcarAnimalComoPerdidoUseCase.java
@@ -1,7 +1,6 @@
 package com.unifio.tcc.track_pet.domain.usecases.animal;
 
 import com.unifio.tcc.track_pet.domain.animal.Animal;
-import com.unifio.tcc.track_pet.domain.sk.AnimalId;
 
 import java.util.UUID;
 
