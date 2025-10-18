3217a31d7272df687c66d785e78a74ec36a20761
Gabriel Messias <gamessiasjt@gmail.com>
2025-10-17 23:44:23 -0300

    feat: criando end point para retornar dados do usuário autenticado

---
+++ src/main/java/com/unifio/tcc/track_pet/domain/usecases/usuario/BuscarDadosUsuarioAutenticadoUseCase.java
@@ -0,0 +1,7 @@
+package com.unifio.tcc.track_pet.domain.usecases.usuario;
+
+import com.unifio.tcc.track_pet.domain.usuario.Usuario;
+
+public interface BuscarDadosUsuarioAutenticadoUseCase {
+    Usuario buscarDadosUsuarioLogado();
+}
